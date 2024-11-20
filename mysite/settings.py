b26bce8b359042c224535ef27bbb7beb88c2ceb4
Lakhdar Belkharroubi <83500639+lakhdar-blk@users.noreply.github.com>
2024-11-20 15:51:11 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Specify the directories containing static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets'),
 ]
 
 # URL to access static files
