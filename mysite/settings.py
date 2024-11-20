03ecedba98cae9c205c503e2f0b4a12a1f61068b
LAKHDAR BELKHARROUBI <lakhdar.belkharroubi@tosyalialgerie.local>
2024-11-20 11:45:20 +0100

    second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,9 +115,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+# Specify the directory where collected static files will be stored
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Specify the directories containing static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# URL to access static files
+STATIC_URL = '/static/'
